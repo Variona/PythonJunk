aeed21b6114f569359fac685929627ce88568319
Variona <5630669+Variona@users.noreply.github.com>
2020-01-25 15:46:13 -0500

    Comments

--- HelloWorld.py
+++ HelloWorld.py
@@ -17,12 +17,12 @@ while True:
 	if temp == x:
 		print(x)
 		break
-	if count >= len(x):
+	if count >= len(x): # If the length is longer than the string x then it will reset and try again.
 		temp = ""
 		count = 0
-	currTime = "{:.0f}".format(time.time() - time0)
+	currTime = "{:.0f}".format(time.time() - time0) # The time since the first call on line 8 limited to whole numbers.
 	totalGuesses += 1
-	prettyTotalGuesses = "{:,d}".format(totalGuesses)
-	if totalGuesses % 76820 == 0:
+	prettyTotalGuesses = "{:,d}".format(totalGuesses) # Turns a hard to read number into a number seperated by commas!
+	if totalGuesses % 76820 == 0: # A decent guestimate of 1 second. Its not really accurate and it can deviate but it works.
 		print(currTime, "second(s) and a total of", prettyTotalGuesses, "guesses.")
 	
