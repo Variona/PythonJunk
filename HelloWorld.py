b6f48aeca166bfccdc156e2eb39ba4e8468c429c
Variona <5630669+Variona@users.noreply.github.com>
2020-01-25 15:44:05 -0500

    Added timer + total guess counter

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,12 @@
 #Quite possibly the best program to print "Hello World" ever.
 from random import randint
 from random import seed
+import time
 x = "Hello World" 
 temp = ""
 count = 0
+time0 = time.time()
+totalGuesses = 0
 print("Starting...")
 
 while True:
@@ -15,6 +18,11 @@ while True:
 		print(x)
 		break
 	if count >= len(x):
-		print(temp)
 		temp = ""
 		count = 0
+	currTime = "{:.0f}".format(time.time() - time0)
+	totalGuesses += 1
+	prettyTotalGuesses = "{:,d}".format(totalGuesses)
+	if totalGuesses % 76820 == 0:
+		print(currTime, "second(s) and a total of", prettyTotalGuesses, "guesses.")
+	
