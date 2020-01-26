982dfaaa0136a4c698748b2f971e55ea627cd688
Variona <5630669+Variona@users.noreply.github.com>
2020-01-25 20:44:51 -0500

    Tabs

--- Guess The Word.py
+++ Guess The Word.py
@@ -20,39 +20,39 @@ def chooseWord():
 
 
 def letterInWord(letter, word, coolList):
-    letter = letter.lower()
-    if letter in word:
-        for i in range(len(word)):
-            if word[i] == letter:
-                coolList[i] = word[i]
+	letter = letter.lower()
+	if letter in word:
+		for i in range(len(word)):
+			if word[i] == letter:
+				coolList[i] = word[i]
 
-    return coolList
+	return coolList
 
 
 while True:
-    count = 0
-    gameWord = chooseWord()
-    maxGuess = len(gameWord) + 3
-    gameList = ["*"] * len(gameWord)
-    print("Welcome to the Word Guessing Game!")
-    while count < maxGuess:
-        print("You have " + str(maxGuess) + " guesses left!")
-        userInput = str(input("Choose your letter!: "))
-        print(letterInWord(userInput, gameWord, gameList))
-        maxGuess -= 1
+	count = 0
+	gameWord = chooseWord()
+	maxGuess = len(gameWord) + 3
+	gameList = ["*"] * len(gameWord)
+	print("Welcome to the Word Guessing Game!")
+	while count < maxGuess:
+		print("You have " + str(maxGuess) + " guesses left!")
+		userInput = str(input("Choose your letter!: "))
+		print(letterInWord(userInput, gameWord, gameList))
+		maxGuess -= 1
 
-        if "*" in gameList:
-            if count == maxGuess:
-                print("You lost!")
-                print("The word was: " + gameWord)
-            continue
-        else:
-            print("Congrats! You won!")
-            break
-    resume = str(input("Do you wish to continue? (Y or N): "))
-    if resume.lower() != "n":
-        count = 0
-        continue
-    else:
-        print("Thanks for playing!")
-        break
+		if "*" in gameList:
+			if count == maxGuess:
+				print("You lost!")
+				print("The word was: " + gameWord)
+			continue
+		else:
+			print("Congrats! You won!")
+			break
+	resume = str(input("Do you wish to continue? (Y or N): "))
+	if resume.lower() != "n":
+		count = 0
+		continue
+	else:
+		print("Thanks for playing!")
+		break
